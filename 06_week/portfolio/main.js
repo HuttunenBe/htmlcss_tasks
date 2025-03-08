7c0478d50bcef2b92129049b47b82889e8b19b9d
Huttunenbe <beniittahuttunen@gmail.com>
2025-03-08 21:54:42 +0200

    add formatting, check file and folder names

--- 06_week/portfolio/main.js
+++ 06_week/portfolio/main.js
@@ -2,8 +2,8 @@ const overlay = document.querySelector(".overlay");
 const backTotopBtn = document.querySelector("#backTotopBtn");
 const closeButton = document.querySelector("#closeButton");
 const modalButton = document.querySelector("#seeMore");
-const modeButton = document.querySelector('#modeButton');
-const body = document.body
+const modeButton = document.querySelector("#modeButton");
+const body = document.body;
 
 window.onscroll = function () {
   scrollFunction();
@@ -29,20 +29,20 @@ const backToTop = () => {
   document.documentElement.scrollTop = 0;
 };
 const toggleLightSwitch = () => {
-    document.body.classList.toggle('darkMode');
+  document.body.classList.toggle("darkMode");
 
-const currentMode = document.body.classList.contains('darkMode'); 
-localStorage.setItem('darkMode', JSON.stringify(currentMode));
-}
+  const currentMode = document.body.classList.contains("darkMode");
+  localStorage.setItem("darkMode", JSON.stringify(currentMode));
+};
 const darkModeOn = JSON.parse(localStorage.getItem("darkMode"));
 
 if (darkModeOn) {
-    document.body.classList.add('darkMode');
+  document.body.classList.add("darkMode");
 } else {
-    document.body.classList.remove('darkMode'); 
+  document.body.classList.remove("darkMode");
 }
 
 backTotopBtn.addEventListener("click", backToTop);
 modalButton.addEventListener("click", displayElement);
 closeButton.addEventListener("click", displayElement);
-modeButton.addEventListener('click', toggleLightSwitch)
+modeButton.addEventListener("click", toggleLightSwitch);
